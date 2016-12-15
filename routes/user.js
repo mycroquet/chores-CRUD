329b399395c585f50a46d38d8a6d3e0d0aed366c
Michael Roque <mikeroque1@gmail.com>
2016-12-15 15:42:33 -0700

    Added user route

---
+++ routes/user.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+const User = require('../db/user');
+
+
+/* GET users listing. */
+router.get('/:id', function(req, res, next) {
+  if(!isNaN(req.params.id)){
+    User.getOne(req.params.id).then(function(user){
+      if (user){
+        delete user.password;
+        res.json(user);
+      } else {
+        resError(res, 404, "User Not Found");
+      }
+    })
+  } else {
+    resError(res, 500, "Invalid ID");
+  }
+});
+
+function resError(res, statusCode, message){
+  res.status(statusCode);
+  res.json({message});
+}
+
+module.exports = router;
